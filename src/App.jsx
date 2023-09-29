5d11251379af8cd5c3cd4b28cf8118c5ddd8874f
prabhavathi2501 <prabhashok2501@gmail.com>
2023-09-29 20:08:09 +0530

    new react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './components/Sidebar'
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return<>
+  <div id='wrapper'>
+  <Sidebar/>
+  <Dashboard/>
+  </div>
+  
+  </>
+}
+
+export default App
